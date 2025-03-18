3f8057684b911d92e9c16c28d0d9e95a6eb2a560
thnhtus <fallforthat@gmail.com>
2025-03-19 00:02:47 +0700

    Add main App component with routing, context provider, and global styles

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import ProductDetails from "./components/ProductDetails";
+import "./App.css";
+
+function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <About />
+                  <Products />
+                  <Contact />
+                </>
+              }
+            />
+            <Route path="/product/:id" element={<ProductDetails />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
